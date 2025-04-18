72cf745585108e96824791ce15e48352fee8fe33
Nitin Sharma <nitin@fuelbuddy.in>
2025-04-18 11:26:50 +0000

    button demo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { FrappeProvider } from 'frappe-react-sdk'
+import { Button } from "@/components/ui/button"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
 	<div className="App">
 	  <FrappeProvider>
+		<Button variant="outline">Click me</Button>
 		<div>
 	  <div>
 		<a href="https://vitejs.dev" target="_blank">
